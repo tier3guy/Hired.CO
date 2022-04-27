c1da2d7bf06fbaae957fc90bd2c82b327e21403d
Avinash Gupta <gavinash6290@gmail.com>
2022-04-27 19:23:29 +0530

    UPDATING LINKS AND ALL

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -4,7 +4,7 @@ export const CodeEditor = () => {
   return(
     <div class="p-0 offcanvas offcanvas-start codeEditor-container" tabindex="-1" id="codeEditor" aria-labelledby="offcanvasExampleLabel">
       <div class="m-0 p-0 offcanvas-body">
-        <iframe class='m-0' src='https://codecollab.io/@proj/AirportTrucksMountain' width='100%' height='115%' id='editor'></iframe>
+        <iframe class='m-0' src='https://codecollab.io/@avinash62/Hired.co' width='100%' height='115%' id='editor'></iframe>
       </div>
     </div>
   );
